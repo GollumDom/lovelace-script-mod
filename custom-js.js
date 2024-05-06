64343aea86490a50b37c33694163f15adb69285b
Damien Duboeuf <smeagolworms4@gmail.com>
2024-05-06 14:53:22 +0200

    Update custom-js.js

--- custom-js.js
+++ custom-js.js
@@ -1,5 +1,5 @@
 console.info(
-	'\n %c Custom JS %c v0.0.1 %c \n',
+	'\n %c Lovelace Script Mod %c v0.0.1 %c \n',
 	'background-color: #555;color: #fff;padding: 3px 2px 3px 3px;border-radius: 3px 0 0 3px;font-family: DejaVu Sans,Verdana,Geneva,sans-serif;text-shadow: 0 1px 0 rgba(1, 1, 1, 0.3)',
 	'background-color: #bc81e0;background-image: linear-gradient(90deg, #b65cff, #11cbfa);color: #fff;padding: 3px 3px 3px 2px;border-radius: 0 3px 3px 0;font-family: DejaVu Sans,Verdana,Geneva,sans-serif;text-shadow: 0 1px 0 rgba(1, 1, 1, 0.3)',
 	'background-color: transparent'
